157cb79dd98b507ec095313ae412c981a90f800a
TalhaShaikh150 <tssmart233@gmail.com>
2025-10-06 02:15:48 +0500

    Fixed Some Bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import "./App.css";
 import Todo from "./Components/Todo";
-import Abc from "./Components/Abc";
-import Api from "./Components/Api";
-import TodoData from "./Components/TodoData";
+// import Abc from "./Components/Abc";
+// import Api from "./Components/Api";
+// import TodoData from "./Components/TodoData";
 
 function App() {
   return (
     <>
-    {/* <Abc/> */}
-    {/* <Api/> */}
+      {/* <Abc/> */}
+      {/* <Api/> */}
       <Todo />
       {/* <TodoData/> */}
     </>
